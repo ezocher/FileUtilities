9997f7e123fdeef3c64324366291738a3b18282a
Eric Zocher <ezocher@users.noreply.github.com>
2021-04-20 21:34:44 -0700

    A little renaming in the CopyUniqueFile class

--- FilesDB/UniqueFileCollector/CopyUniqueFile.cs
+++ FilesDB/UniqueFileCollector/CopyUniqueFile.cs
@@ -9,20 +9,25 @@ namespace UniqueFileCollector
 {
     class CopyUniqueFile
     {
-        static string destBasePath = @"D:\uu-unspecified";
+        const string destVolumeLetter = @"F:\";
+        const string destRootPrefix = "uu-";
+
+        static string destBasePath = destVolumeLetter + destRootPrefix + "unspecified";
 
         public static void SetSourceVolumeName(string name)
         {
-            destBasePath = @"D:\uu-" + name;
+            destBasePath = destVolumeLetter + destRootPrefix + name;
         }
 
-        public static void Copy(string filePath)
+        public static void Copy(string sourceFilePath)
         {
-            string destFilePath = destBasePath + filePath.Remove(0, 2);    // Remove "X:"
+            string destFilePath = destBasePath + sourceFilePath.Remove(0, 2);    // Remove "X:"
             string destDirPath = Path.GetDirectoryName(destFilePath);
 
             Directory.CreateDirectory(destDirPath);
-            File.Copy(filePath, destFilePath, false);
+            File.Copy(sourceFilePath, destFilePath, false);
+            // System.IO.IOException: 'The file 'F: \uu - ZB - DriveC\Users\ezoch\Desktop\temp.html' already exists.'
+            // System.IO.FileNotFoundException: 'Could not find file 'C:\Users\ezoch\Desktop\LEFT MON\!Left DT - XMas\California wildfires- Is Trump right when he blames forest managers- - BBC News'.'
         }
     }
 }
