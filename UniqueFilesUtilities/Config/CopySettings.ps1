2b16d76a244ad09950775a1509d2e7d43b0e7590
Eric Zocher <ezocher@live.com>
2026-08-30 11:51:51 -0700

    Add description to CopySettings.ps1

--- UniqueFilesUtilities/Config/CopySettings.ps1
+++ UniqueFilesUtilities/Config/CopySettings.ps1
@@ -1,1 +1,4 @@
+# Copies all of the settings files from the solution's Config folder to the live settings location:
+#	C:\Users\username\OneDrive\Files and Storage\AppConfig
+
 Copy-Item -Force -Path "Config\*.txt" -Destination "C:\Users\ezoch\OneDrive\Files and Storage\AppConfig"
